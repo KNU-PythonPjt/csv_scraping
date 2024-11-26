f53d2a69a20ae20f25b961046b1e542b82725a10
noojmogstel <goaudgo@naver.com>
2024-11-26 15:20:02 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+# def download_csv(url, save_path):
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         os.makedirs(os.path.dirname(save_path), exist_ok=True)
+#         with open
+
+print(os.getcwd())
